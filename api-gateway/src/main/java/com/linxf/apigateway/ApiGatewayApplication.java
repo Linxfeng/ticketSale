85d78df654403cfc89f73f8959551d5bba30efbd
lintao <1538063845@qq.com>
2019-03-26 16:46:28 +0800

    实现zuul的动态路由

--- api-gateway/src/main/java/com/linxf/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/linxf/apigateway/ApiGatewayApplication.java
@@ -2,7 +2,10 @@ package com.linxf.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,6 +13,13 @@ public class ApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    //实现zuul的动态路由
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties() {
+        return new ZuulProperties();
     }
 
 }
