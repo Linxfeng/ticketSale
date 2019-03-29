60fcd3a5e61c8ee00546c455ae33412e25394466
lintao <1538063845@qq.com>
2019-03-29 12:52:50 +0800

    完善用户登陆、注册功能

--- user/src/main/java/com/linxf/user/controller/UserController.java
+++ user/src/main/java/com/linxf/user/controller/UserController.java
@@ -9,6 +9,7 @@ import com.linxf.user.utils.RedisCacheUtil;
 import com.linxf.user.utils.VerifyParamsUtil;
 import com.linxf.user.vo.UserVo;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,6 +41,7 @@ public class UserController {
     @PostMapping("/register")
     public ResponseVo register(UserVo userVo, HttpServletResponse response) {
         UserInfo userInfo = new UserInfo();
+        response.addHeader("Access-Control-Allow-Origin", "*");//CORS跨域
         try {
             VerifyParamsUtil.validUserVo(userVo); //校验参数
             UserInfo user = userInfoService.findByPhone(userVo.getPhone());
@@ -59,6 +61,34 @@ public class UserController {
             return ResponseVo.failed(e.getMessage());
         }
         return ResponseVo.success("注册成功！");
+    }
+
+    /**
+     * 用户登陆
+     * @param userVo
+     * @return
+     */
+    @PostMapping("/login")
+    public ResponseVo login(UserVo userVo, HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");//CORS跨域
+        try {
+            VerifyParamsUtil.validUserVo(userVo); //校验参数
+            UserInfo userInfo = userInfoService.findByPhone(userVo.getPhone());
+            if (userInfo == null)
+                return ResponseVo.failed("登陆失败！号码" + userInfo.getPhone() + "未注册");
+            if (!StringUtils.equals(userVo.getUsername(), userInfo.getUsername()))
+                return ResponseVo.failed("登陆失败！该号码对应的用户名为：" + userInfo.getUsername());
+            if (!StringUtils.equals(userVo.getPassword(), userInfo.getPassword()))
+                return ResponseVo.failed("登陆失败！密码错误");
+            String token = redisCacheUtil.setAndReturnUUID(userInfo.getUid());//用户信息写入redis
+            response.addHeader("uid", userInfo.getUid());//用户信息uid写入浏览器响应头
+            response.addHeader("u_token", token);//认证信息token写入浏览器响应头
+        } catch (Exception e) {
+            log.error("【用户登陆】失败, {}", userVo);
+            log.error("UserController.login ERROR:{}", e.getMessage());
+            return ResponseVo.failed(e.getMessage());
+        }
+        return ResponseVo.success("登陆成功！");
     }
 
 }
