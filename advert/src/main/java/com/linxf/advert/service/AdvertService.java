5e9bd02bbf9fc917da2d9d484f308f17f4b03b1a
lintao <1538063845@qq.com>
2019-03-31 15:34:10 +0800

    查询所有有效/失效广告列表

--- advert/src/main/java/com/linxf/advert/service/AdvertService.java
+++ advert/src/main/java/com/linxf/advert/service/AdvertService.java
@@ -1,8 +1,19 @@
 package com.linxf.advert.service;
 
+import com.linxf.advert.dataobject.Advert;
+
+import java.util.List;
+
 /**
  * @Author lintao
  * @Date 2019/3/31
  */
 public interface AdvertService {
+
+    /**
+     * 查询所有有效/失效广告列表
+     * @param del 0-有效,1-失效
+     * @return
+     */
+    List<Advert> getAllAdvert(Integer del);
 }
--- advert/src/main/java/com/linxf/advert/service/impl/AdvertServiceImpl.java
+++ advert/src/main/java/com/linxf/advert/service/impl/AdvertServiceImpl.java
@@ -1,7 +1,12 @@
 package com.linxf.advert.service.impl;
 
+import com.linxf.advert.dataobject.Advert;
+import com.linxf.advert.repository.AdvertRepository;
 import com.linxf.advert.service.AdvertService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 广告服务
@@ -10,4 +15,17 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AdvertServiceImpl implements AdvertService {
+
+    @Autowired
+    private AdvertRepository advertRepository;
+
+    /**
+     * 查询所有有效/失效广告列表
+     * @param del 0-有效,1-失效
+     * @return
+     */
+    @Override
+    public List<Advert> getAllAdvert(Integer del) {
+        return advertRepository.findAllByDel(del);
+    }
 }
