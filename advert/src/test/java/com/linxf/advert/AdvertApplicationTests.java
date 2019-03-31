e9bf8b207dd5ad1d656e82edfe8139299907e35c
lintao <1538063845@qq.com>
2019-03-31 14:54:24 +0800

    广告服务：advert

---
+++ advert/src/test/java/com/linxf/advert/AdvertApplicationTests.java
@@ -0,0 +1,16 @@
+package com.linxf.advert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvertApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
