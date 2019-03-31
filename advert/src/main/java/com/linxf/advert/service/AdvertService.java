0ba52a3fabb860d7840f0b0185335ee31ca501be
lintao <1538063845@qq.com>
2019-03-31 15:14:40 +0800

    新增广告服务类AdvertService

---
+++ advert/src/main/java/com/linxf/advert/service/AdvertService.java
@@ -0,0 +1,8 @@
+package com.linxf.advert.service;
+
+/**
+ * @Author lintao
+ * @Date 2019/3/31
+ */
+public interface AdvertService {
+}
---
+++ advert/src/main/java/com/linxf/advert/service/impl/AdvertServiceImpl.java
@@ -0,0 +1,13 @@
+package com.linxf.advert.service.impl;
+
+import com.linxf.advert.service.AdvertService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 广告服务
+ * @Author lintao
+ * @Date 2019/3/31
+ */
+@Service
+public class AdvertServiceImpl implements AdvertService {
+}
