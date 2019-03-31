315f797c390d34f7134872877e9fab242d2539e1
lintao <1538063845@qq.com>
2019-03-31 15:39:15 +0800

    获取有效广告列表

--- advert/src/main/java/com/linxf/advert/controller/AdvertController.java
+++ advert/src/main/java/com/linxf/advert/controller/AdvertController.java
@@ -1,5 +1,7 @@
 package com.linxf.advert.controller;
 
+import com.linxf.advert.dataobject.Advert;
+import com.linxf.advert.service.AdvertService;
 import com.linxf.common.utils.RedisCacheUtil;
 import com.linxf.common.vo.ResponseVo;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author lintao
@@ -19,15 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdvertController {
 
     @Autowired
+    private AdvertService advertService;
+
+    @Autowired
     private RedisCacheUtil redisCacheUtil;
 
     @GetMapping("/showAllAdvert")
     public ResponseVo getAllAdvert() {
+        List<Advert> advertList = null;
         //先从缓存中取
         String value = redisCacheUtil.getValue("");
         if (StringUtils.isBlank(value)) { //如果缓存中没有泽查数据库
-
+            advertList = advertService.getAllAdvert(0);
         }
-        return ResponseVo.failed("查询失败！");
+        return ResponseVo.success("查询成功", advertList);
     }
 }
