56bd720b880cc3e2c8a00a7f3825ba8869c32ed7
lintao <1538063845@qq.com>
2019-04-01 13:15:04 +0800

    新增方法：根据id查询

--- advert/src/main/java/com/linxf/advert/controller/AdvertController.java
+++ advert/src/main/java/com/linxf/advert/controller/AdvertController.java
@@ -1,7 +1,9 @@
 package com.linxf.advert.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.linxf.advert.dataobject.Advert;
 import com.linxf.advert.service.AdvertService;
+import com.linxf.common.utils.JsonUtil;
 import com.linxf.common.utils.RedisCacheUtil;
 import com.linxf.common.vo.ResponseVo;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -22,20 +25,54 @@ import java.util.List;
 @RequestMapping("/advert")
 public class AdvertController {
 
+    private static final String ADVERTLIST0 = "advertList0";
+    private static final String ADVERTLIST1 = "advertList1";
+    private static final String ADVERTID = "advert_id_";
+
     @Autowired
     private AdvertService advertService;
 
     @Autowired
     private RedisCacheUtil redisCacheUtil;
 
+    /**
+     * 查询所有有效广告列表
+     */
     @GetMapping("/showAllAdvert")
-    public ResponseVo getAllAdvert() {
+    public ResponseVo getAllAdvert(HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");//CORS跨域
         List<Advert> advertList = null;
         //先从缓存中取
-        String value = redisCacheUtil.getValue("");
-        if (StringUtils.isBlank(value)) { //如果缓存中没有泽查数据库
+        String value = redisCacheUtil.getValue(ADVERTLIST0);
+        //如果缓存中有，则转换为list，若没有没有则查数据库
+        if (StringUtils.isNotBlank(value)) {
+            advertList = (List<Advert>) JsonUtil.jsonToObject(value,
+                    new TypeReference<List<Advert>>() {});
+        } else {
             advertList = advertService.getAllAdvert(0);
+            redisCacheUtil.setValue(ADVERTLIST0, JsonUtil.toJson(advertList));
         }
         return ResponseVo.success("查询成功", advertList);
     }
+
+    /**
+     * 查询单个advert信息
+     * @param id
+     */
+    @GetMapping("/advertInfo")
+    public ResponseVo getAdvert(Integer id, HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");//CORS跨域
+        if (id == null || id == 0)
+            return ResponseVo.failed("参数不能为空");
+        Advert advertInfo = null;
+        String value = redisCacheUtil.getValue(ADVERTID+id);
+        if (StringUtils.isNotBlank(value)) {
+            advertInfo = (Advert) JsonUtil.jsonToObject(value, new TypeReference<Advert>() {});
+        } else {
+            advertInfo = advertService.getAdvertById(id);
+            redisCacheUtil.setValue(ADVERTID+id, JsonUtil.toJson(advertInfo));
+        }
+        return ResponseVo.success("查询成功", advertInfo);
+    }
+
 }
