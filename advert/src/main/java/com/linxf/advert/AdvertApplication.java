6726832a2185a666ac8f2f111506f0e02e6e221b
lintao <1538063845@qq.com>
2019-04-01 13:07:50 +0800

    添加扫描包路径，保证能使用common的工具类

--- advert/src/main/java/com/linxf/advert/AdvertApplication.java
+++ advert/src/main/java/com/linxf/advert/AdvertApplication.java
@@ -3,9 +3,11 @@ package com.linxf.advert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages={"com.linxf.advert.*", "com.linxf.common.*"})
 public class AdvertApplication {
 
     public static void main(String[] args) {
