7d589e6b2d528237eaa9fea72f1e5a4e75e2ad26
lintao <1538063845@qq.com>
2019-04-01 13:07:36 +0800

    添加扫描包路径，保证能使用common的工具类

--- user/src/main/java/com/linxf/user/UserApplication.java
+++ user/src/main/java/com/linxf/user/UserApplication.java
@@ -3,9 +3,11 @@ package com.linxf.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages={"com.linxf.user.*", "com.linxf.common.*"})
 public class UserApplication {
 
     public static void main(String[] args) {
