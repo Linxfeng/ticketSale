d5118c3cd16721af5a7dd88eba4a2e7a8eb38271
lintao <1538063845@qq.com>
2019-04-07 22:05:58 +0800

    测试用例：新增乘客

--- user/src/test/java/com/linxf/user/service/impl/PassengerServiceImplTest.java
+++ user/src/test/java/com/linxf/user/service/impl/PassengerServiceImplTest.java
@@ -1,7 +1,9 @@
 package com.linxf.user.service.impl;
 
+import com.linxf.common.utils.UUIDUtil;
 import com.linxf.user.UserApplicationTests;
 import com.linxf.user.dataobject.Passenger;
+import com.linxf.user.enums.PassengerTypeEnum;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,8 +18,19 @@ public class PassengerServiceImplTest extends UserApplicationTests {
         passenger.setUid("1554086717367104");
         passenger.setTrueName("测试");
         passenger.setIdCard("123456789012345678");
-        passenger.setRole(0);
+        passenger.setRole(PassengerTypeEnum.ADULT.getCode());
         Passenger p = passengerService.findPassenger(passenger);
         System.out.println(p);
+    }
+
+    @Test
+    public void createUser() {
+        Passenger passenger = new Passenger();
+        passenger.setUid("1554086717367104");
+        passenger.setPid(UUIDUtil.get16UUID());
+        passenger.setTrueName("测试");
+        passenger.setIdCard("123456789012345678");
+        passenger.setRole(PassengerTypeEnum.ADULT.getCode());
+        passengerService.createPassenger(passenger);
     }
 }
