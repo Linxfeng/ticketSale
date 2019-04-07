f9863c2309b573e426d2fa5d70f0a0c464765155
lintao <1538063845@qq.com>
2019-04-07 21:54:03 +0800

    乘客信息服务-根据乘客信息查询

--- user/src/main/java/com/linxf/user/service/impl/PassengerServiceImpl.java
+++ user/src/main/java/com/linxf/user/service/impl/PassengerServiceImpl.java
@@ -1,7 +1,13 @@
 package com.linxf.user.service.impl;
 
 import com.linxf.user.dataobject.Passenger;
+import com.linxf.user.repository.PassengerRepository;
 import com.linxf.user.service.PassengerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * 乘客服务实现类
@@ -9,7 +15,11 @@ import com.linxf.user.service.PassengerService;
  * @Author lintao
  * @Date 2019/4/7
  */
+@Service
 public class PassengerServiceImpl implements PassengerService {
+
+    @Autowired
+    private PassengerRepository passengerRepository;
 
     /**
      * 根据乘客信息查询
@@ -19,6 +29,9 @@ public class PassengerServiceImpl implements PassengerService {
      */
     @Override
     public Passenger findPassenger(Passenger passenger) {
-        return null;
+        Example<Passenger> example = Example.of(passenger);
+        Optional<Passenger> optional = passengerRepository.findOne(example);
+        if (!optional.isPresent()) return null;
+        return optional.get();
     }
 }
