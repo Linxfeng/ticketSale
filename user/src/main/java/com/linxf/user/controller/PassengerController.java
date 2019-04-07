161c3729dbf2b12b5d84aebc608c8502c108c242
lintao <1538063845@qq.com>
2019-04-07 22:24:27 +0800

    完善新增乘客信息

--- user/src/main/java/com/linxf/user/controller/PassengerController.java
+++ user/src/main/java/com/linxf/user/controller/PassengerController.java
@@ -1,12 +1,15 @@
 package com.linxf.user.controller;
 
+import com.linxf.common.utils.UUIDUtil;
 import com.linxf.common.vo.ResponseVo;
 import com.linxf.user.dataobject.Passenger;
+import com.linxf.user.service.PassengerService;
 import com.linxf.user.utils.VerifyParamsUtil;
 import com.linxf.user.vo.PassengerVo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,9 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @RequestMapping("/passenger")
 public class PassengerController {
+
+    @Autowired
+    private PassengerService passengerService;
 
     /**
      * 新增乘客
@@ -43,10 +49,15 @@ public class PassengerController {
             Passenger passenger = new Passenger();
             BeanUtils.copyProperties(passengerVo, passenger);
             passenger.setUid(uid);
-            //先根据乘客信息查询
 
+            /**这里存在一个bug，同一身份证号的乘客原则上不允许重复添加，但又分学生和成人，这里暂放*/
+            //先根据乘客信息查询，若该乘客已存在，则提示
+            if (passengerService.findPassenger(passenger) != null) {
+                return ResponseVo.failed("该乘客已存在您的乘客列表中");
+            }
             //新增乘客
-
+            passenger.setPid(UUIDUtil.get16UUID());
+            passengerService.createPassenger(passenger);
         } catch (Exception e) {
             log.error("【新增乘客】失败, {}", passengerVo);
             log.error("PassengerController.createPassenger ERROR:{}", e.getMessage());
