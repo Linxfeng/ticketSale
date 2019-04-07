30501367b14cc9d804ad4f700bc2ace9e0c4c8d4
lintao <1538063845@qq.com>
2019-04-07 21:31:19 +0800

    新增 校验乘客vo表单

--- user/src/main/java/com/linxf/user/utils/VerifyParamsUtil.java
+++ user/src/main/java/com/linxf/user/utils/VerifyParamsUtil.java
@@ -1,5 +1,6 @@
 package com.linxf.user.utils;
 
+import com.linxf.user.vo.PassengerVo;
 import com.linxf.user.vo.UserVo;
 import org.springframework.util.Assert;
 
@@ -19,6 +20,16 @@ public class VerifyParamsUtil {
         Assert.notNull(userVo.getUsername(), "用户名不能为空！");
         Assert.notNull(userVo.getPassword(), "密码不能为空！");
         Assert.notNull(userVo.getPhone(), "电话号码不能为空！");
+    }
+
+    /**
+     * 校验乘客vo表单
+     */
+    public static void validPassengerVo(PassengerVo passengerVo) {
+        Assert.notNull(passengerVo, "参数不能为空！");
+        Assert.notNull(passengerVo.getTrueName(), "乘客姓名不能为空！");
+        Assert.notNull(passengerVo.getIdCard(), "身份证号不能为空！");
+        Assert.notNull(passengerVo.getRole(), "乘客类型不能为空！");
     }
 
 }
