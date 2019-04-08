e2742183767a670aa86df58eedcbef980ee2894d
lintao <1538063845@qq.com>
2019-04-08 10:29:00 +0800

    乘客Dao层新增：查询用户的有效乘客列表

--- user/src/main/java/com/linxf/user/repository/PassengerRepository.java
+++ user/src/main/java/com/linxf/user/repository/PassengerRepository.java
@@ -3,6 +3,8 @@ package com.linxf.user.repository;
 import com.linxf.user.dataobject.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * 乘客Dao层
  *
@@ -10,5 +12,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @Date 2019/4/7
  */
 public interface PassengerRepository extends JpaRepository<Passenger, String> {
+
+    /**
+     * 查询用户的乘客列表
+     *
+     * @param uid 用户id
+     * @param del 是否有效，0-有效，1-失效
+     * @return
+     */
+    List<Passenger> findByUidAndDel(String uid, Integer del);
 
 }
