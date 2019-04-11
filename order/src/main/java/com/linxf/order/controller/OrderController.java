98ce61e9481208cf3c5ee83bd064281e662df1bd
lintao <1538063845@qq.com>
2019-04-11 18:49:01 +0800

    订单服务-提交订单Controller

---
+++ order/src/main/java/com/linxf/order/controller/OrderController.java
@@ -0,0 +1,18 @@
+package com.linxf.order.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单Controller
+ *
+ * @author lintao
+ * @date 2019/4/11
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class OrderController {
+
+}
