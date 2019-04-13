7bf8ef848c4382ff1a0ed97129a8c30673bffc70
lintao <1538063845@qq.com>
2019-04-13 10:50:36 +0800

    提交列车实体类Train

---
+++ ticket/src/main/java/com/linxf/ticket/dataobject/Train.java
@@ -0,0 +1,43 @@
+package com.linxf.ticket.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * 列车信息
+ *
+ * @author lintao
+ * @date 2019/4/13
+ */
+@Data
+@Entity
+public class Train {
+
+    @Id
+    private String tid;// 列车车次编号
+
+    private String trainType;// 车辆类型
+
+    //一般列车都会分3个档次的座位类型
+    private String siteType1;// 座位类型1
+    private Integer lastTicket1;// 座位余票1
+    private Double money1;// 票价涨幅1
+
+    private String siteType2;// 座位类型2
+    private Integer lastTicket2;// 座位余票2
+    private Double money2;// 票价涨幅2
+
+    private String siteType3;// 座位类型3
+    private Integer lastTicket3;// 座位余票3
+    private Double money3;// 票价涨幅3
+
+    private String driveTime;// 车程耗时-单位:小时
+
+    private Integer stationSum;// 列车途经总站数
+
+    private Integer saleSum;// 售出总票数
+
+    private Integer del = 0;// 是否有效：0在运营，1已停运
+}
