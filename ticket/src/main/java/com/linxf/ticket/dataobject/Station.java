db8061bb94b347921b568bd6b783c441b30ed63d
lintao <1538063845@qq.com>
2019-04-13 11:17:24 +0800

    提交车站实体类Station

---
+++ ticket/src/main/java/com/linxf/ticket/dataobject/Station.java
@@ -0,0 +1,35 @@
+package com.linxf.ticket.dataobject;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import java.sql.Time;
+
+/**
+ * 车站
+ *
+ * @author lintao
+ * @date 2019/4/13
+ */
+@Data
+@Entity
+public class Station {
+
+    private String tid;// 车次编号
+
+    private String name1;// 出发站
+
+    private Time time1;// 出站时间 //Time.valueOf("11:12:00")
+
+    private String name2;// 到达站--是name1的下一站
+
+    private Time time2;// 到站时间
+
+    private Integer isNextDay;//是否次日到达：0当天，1次日
+
+    private Integer ticket;// 区段余票
+
+    private Double money;// 区段票价
+
+}
