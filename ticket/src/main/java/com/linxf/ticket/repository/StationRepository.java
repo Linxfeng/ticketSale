0356ca557e1150cddb289d5ddf5ef39b75010936
lintao <1538063845@qq.com>
2019-04-17 16:43:12 +0800

    新增方法：查询某辆列车的车站列表

--- ticket/src/main/java/com/linxf/ticket/repository/StationRepository.java
+++ ticket/src/main/java/com/linxf/ticket/repository/StationRepository.java
@@ -3,6 +3,8 @@ package com.linxf.ticket.repository;
 import com.linxf.ticket.dataobject.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * 车站Dao层
  *
@@ -10,4 +12,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @date 2019/4/16
  */
 public interface StationRepository extends JpaRepository<Station, String> {
+
+    /**
+     * 查询某辆列车的车站列表
+     *
+     * @param tid
+     * @return
+     */
+    List<Station> findAllByTid(String tid);
 }
