6e163321e94a0e8e6808e2888822500686cc82c7
lintao <1538063845@qq.com>
2019-04-17 16:23:18 +0800

    新增方法：根据tid查询车辆信息

--- ticket/src/main/java/com/linxf/ticket/controller/TrainController.java
+++ ticket/src/main/java/com/linxf/ticket/controller/TrainController.java
@@ -10,6 +10,7 @@ import com.linxf.ticket.vo.TrainVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -84,6 +85,26 @@ public class TrainController {
             return ResponseVo.success("查询成功！", tidList);
         } catch (Exception e) {
             log.error("TrainController.listTid ERROR:{}", e.getMessage());
+            return ResponseVo.failed(e.getMessage());
+        }
+    }
+
+    /**
+     * 根据tid获取车辆信息
+     *
+     * @param tid
+     * @return
+     */
+    @PostMapping("/trainInfo")
+    public ResponseVo trainInfo(String tid, HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", "*");//CORS跨域
+        Assert.notNull(tid, "参数tid不能为空！");
+        try {
+            Train train = trainService.getTrainInfo(tid);
+            if (train == null) return ResponseVo.noDataFailed("未查询到该车辆信息");
+            return ResponseVo.success("查询成功！", train);
+        } catch (Exception e) {
+            log.error("TrainController.trainInfo ERROR:{}", e.getMessage());
             return ResponseVo.failed(e.getMessage());
         }
     }
