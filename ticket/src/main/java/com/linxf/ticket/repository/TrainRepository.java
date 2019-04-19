06fe0ad3a7103324fdcf91c65e23cd2bcdaf8e5f
lintao <1538063845@qq.com>
2019-04-19 10:20:30 +0800

    新增查询：根据车辆类型查询车辆列表

--- ticket/src/main/java/com/linxf/ticket/repository/TrainRepository.java
+++ ticket/src/main/java/com/linxf/ticket/repository/TrainRepository.java
@@ -3,6 +3,8 @@ package com.linxf.ticket.repository;
 import com.linxf.ticket.dataobject.Train;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * 车辆Dao层
  *
@@ -10,4 +12,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @date 2019/4/16
  */
 public interface TrainRepository extends JpaRepository<Train, String> {
+
+    /**
+     * 根据车辆类型查询车辆列表
+     *
+     * @param trainType
+     * @return
+     */
+    List<Train> findAllByTrainType(String trainType);
 }
