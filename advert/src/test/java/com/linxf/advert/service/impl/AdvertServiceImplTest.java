fec97b42ee1b1b478288c2727c1466af997d0f7f
lintao <1538063845@qq.com>
2019-04-23 10:07:47 +0800

    测试用例：新增公告

---
+++ advert/src/test/java/com/linxf/advert/service/impl/AdvertServiceImplTest.java
@@ -0,0 +1,23 @@
+package com.linxf.advert.service.impl;
+
+import com.linxf.advert.AdvertApplicationTests;
+import com.linxf.advert.dataobject.Advert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+public class AdvertServiceImplTest extends AdvertApplicationTests {
+
+    @Autowired
+    private AdvertServiceImpl advertService;
+
+    @Test
+    public void addAdvert() {
+        Advert advert = new Advert();
+        advert.setTitle("标题");
+        advert.setContent("内容");
+        advert.setCreateTime(new Date());
+        advertService.addAdvert(advert);
+    }
+}
