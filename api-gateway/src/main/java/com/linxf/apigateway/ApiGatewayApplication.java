6d3d777f31835109c461dbf1fd93a25b1e3d87f0
lintao <1538063845@qq.com>
2019-04-23 16:49:29 +0800

    服务网关：添加鉴权和限流过滤器

--- api-gateway/src/main/java/com/linxf/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/linxf/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,20 @@
 package com.linxf.apigateway;
 
+import com.linxf.apigateway.filter.AuthenticationFilter;
+import com.linxf.apigateway.filter.RateLimitFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages={"com.linxf.apigateway.*", "com.linxf.common.*"})
 @EnableZuulProxy
 public class ApiGatewayApplication {
 
@@ -20,6 +27,22 @@ public class ApiGatewayApplication {
     @RefreshScope
     public ZuulProperties zuulProperties() {
         return new ZuulProperties();
+    }
+
+    /**
+     * 限流过滤器
+     */
+    @Bean
+    public RateLimitFilter rateLimitFilter() {
+        return new RateLimitFilter();
+    }
+
+    /**
+     * 鉴权过滤器
+     */
+    @Bean
+    public AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
     }
 
 }
