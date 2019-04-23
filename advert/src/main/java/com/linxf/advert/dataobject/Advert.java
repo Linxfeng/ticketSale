4b107dd0234243ffc9e567efcd5149f4d131c0c3
lintao <1538063845@qq.com>
2019-04-23 10:06:51 +0800

    公告对象，设置主键id自增

--- advert/src/main/java/com/linxf/advert/dataobject/Advert.java
+++ advert/src/main/java/com/linxf/advert/dataobject/Advert.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
@@ -18,7 +19,7 @@ import java.util.Date;
 public class Advert {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String title;//标题
