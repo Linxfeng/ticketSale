0efd220ca82cacdcdafd69bc19a4039b6a532840
lintao <1538063845@qq.com>
2019-04-27 15:07:02 +0800

    测试用例：创建订单

---
+++ order/src/test/java/com/linxf/order/service/impl/OrderServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.linxf.order.service.impl;
+import java.util.Date;
+
+import com.linxf.common.utils.UUIDUtil;
+import com.linxf.order.OrderApplicationTests;
+import com.linxf.order.dataobject.Order;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 订单服务测试用例
+ *
+ * @Author lintao
+ * @Date 2019/4/27
+ */
+public class OrderServiceImplTest extends OrderApplicationTests {
+
+    @Autowired
+    private OrderServiceImpl orderService;
+
+    @Test
+    public void createOrder() {
+        Order order = new Order();
+        order.setOid(UUIDUtil.get16UUID());
+        order.setUid("1554086717367104");
+        order.setPid("1554645635284698");
+        order.setTid("D2262");
+        order.setStartStation("上海");
+        order.setStartTime(new Date());
+        order.setEndStation("苏州");
+        order.setEndTime(new Date());
+        order.setSeatType(0);
+        order.setAmount(0.0D);
+        order.setOrderStatus(0);
+        order.setCreateTime(new Date());
+        order.setUpdateTime(new Date());
+        order.setDel(0);
+        orderService.createOrder(order);
+    }
+}
