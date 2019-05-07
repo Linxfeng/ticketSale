8c5488f19c145666fa9c9b7f64c8ac58c7e12cd3
lintao <1538063845@qq.com>
2019-05-07 10:57:59 +0800

    测试用例：根据到达站查询所有车次列表

--- ticket/src/test/java/com/linxf/ticket/repository/StationRepositoryTest.java
+++ ticket/src/test/java/com/linxf/ticket/repository/StationRepositoryTest.java
@@ -14,8 +14,9 @@ public class StationRepositoryTest extends TrainApplicationTests {
     @Test
     public void findTidByName1() {
         List<String> list = stationRepository.findTidByName1("上海");
-        if (list != null && list.size() != 0)
-            for (String tid : list) {
+        List<String> list2 = stationRepository.findTidByName2("杭州");
+        if (list2 != null && list2.size() != 0)
+            for (String tid : list2) {
                 System.out.println(tid);
             }
         System.out.println("null");
